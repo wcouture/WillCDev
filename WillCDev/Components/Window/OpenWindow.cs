c39e5b5744f53163823e17fc94a8051f0966ac14
Couture, William <Couture.William@flsenate.gov>
2026-08-24 16:31:20 -0400

    Adds task bar and window framework

---
+++ WillCDev/Components/Window/OpenWindow.cs
@@ -0,0 +1,18 @@
+﻿namespace WillCDev.Components.Window
+{
+    public enum Programs
+    {
+        StartMenu,
+        Settings,
+        AboutMe,
+        Projects,
+        Contact,
+    }
+
+    public class OpenWindow
+    {
+        public int ID { get; set; } = 0;
+        public string WindowTitle { get; set; } = "Window Title";
+        public Programs Program { get; set; } = Programs.StartMenu;
+    }
+}
