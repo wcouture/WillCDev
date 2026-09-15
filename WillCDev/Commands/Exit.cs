d74f59cba0a304765f582159900093b22957d5e7
Couture, William <Couture.William@flsenate.gov>
2026-09-15 15:52:41 -0400

    Adds exit command

---
+++ WillCDev/Commands/Exit.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Attributes;
+using Shared.Services;
+
+namespace WillCDev.Commands
+{
+    [Command("exit")]
+    public class Exit(IApplicationService applicationService) : CommandHandlerBase
+    {
+        public override Task HandleCommand(int AppId, List<string> consoleLines, params string[] args)
+        {
+            applicationService.StopApplication(AppId);
+            return Task.CompletedTask;
+        }
+    }
+}
